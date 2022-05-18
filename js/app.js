a3e0e38e89e63f38b33d00f9fc9a4df8b741e3b6
rdzPedraos <juancursosrodriguez@gmail.com>
2022-05-18 10:57:49 -0500

    version actual

--- js/app.js
+++ js/app.js
@@ -1,26 +1,36 @@
-import { createRejilla, colors as colorPixel } from './layout.js';
+import { setInterfaz, colors as colorPixel } from './layout.js';
 import { mouseMovement } from './events.js';
 
 //define globals variables
 var sizeP,      //Tamaño de un pixel: {x:, y:}
     div,        //Div que contiene el canvas.
     canvas,     //Objeto html del Canvas.
-    ctx;        //Contexto del canvas.
+    ctx,        //Contexto del canvas.
+    dataMatriz,
+    start = false,
+    failed = false
 //------------------------
 
 window.onload = () => {
-    sizeP = { x: 50, y: 50 };
+    const nPixels = { cols: 20, rows: 18};
+    sizeP = { x: 40, y: 40 };
     
     div = document.getElementById('game');
     canvas = document.getElementById('canvas-game');
     ctx = canvas.getContext('2d');
+    dataMatriz = defineMatriz(nPixels.cols, nPixels.rows);
 
-    resizeScreen( 10, 8 );
+    resizeScreen(nPixels.cols, nPixels.rows);
+    setInterfaz(ctx, sizeP, dataMatriz);
 
     canvas.addEventListener('mousemove', (ev) => {
-        resizeScreen( 10, 8 );
+        setInterfaz(ctx, sizeP, dataMatriz);
         mouseMovement(ctx, sizeP, colorPixel.hover, ev);
     });
+
+    canvas.addEventListener('click', (ev)=>{
+        console.log(ev);
+    })
 }
 
 
@@ -36,9 +46,39 @@ const resizeScreen = (cols, rows) => {
 
     canvas.height = height;
     canvas.width = width;
-
-    createRejilla(canvas, sizeP);
 }
 
+const defineMatriz = (cols, rows) => {
+    const matriz = [];
+    for(let i=0; i < rows; i++){
+        matriz.push([]);
+        for(let j=0; j < cols; j++){
+            matriz[i].push(null);
+        }
+    }
 
+    return matriz;
+}
 //--------------------------------;
+
+
+
+
+const debug = (dataMatriz) => {
+    dataMatriz[1][2] = 1;
+    dataMatriz[1][3] = 2;
+    dataMatriz[1][4] = 0;
+
+    dataMatriz[2][3] = 3;
+    dataMatriz[2][4] = 4;
+    dataMatriz[2][6] = 'b';
+    dataMatriz[2][7] = 'b';
+    dataMatriz[2][8] = 'f';
+    dataMatriz[2][9] = 5;
+
+    dataMatriz[3][6] = 'b';
+    dataMatriz[3][7] = 'b';
+
+    dataMatriz[4][6] = 'b';
+    dataMatriz[4][7] = 'b';
+}
--- js/layout.js
+++ js/layout.js
@@ -1,31 +1,107 @@
 export const colors = {
-    0: '#aad751', 
-    1: '#a2d149', 
-    hover: '#bfe07d'
+    default_pixels: ['#cccccc', '#fff'],
+    
+    noOpen: '#aaee33bb',
+    open: '#ffc395e0', //ddc395e0
+    hover: '#fff5',
+
+    text: ['', '#5555ff', '#558855', 'red', 'brown', 'purple'],
+    bombs: ['#db3236', '#008744', '#f4840d', '#ed44b5', '#48e6f1', '#f4c20d', '#b648f2', '#4885ed']
 }; 
 
 
-/**
- * Imprime en el canvas una rejilla que contiene una cuadricula con las dimensiones dadas.
- * @param {HTMLObjectElement} cv Objeto canvas referido al html.
- * @param { {x:int, y:int} } szPixel Objeto con las medidas que tendrá un pixel en x y y.
- */
-export function createRejilla(cv, szPixel){
+export function setInterfaz(ctx, szPixel, matriz){
+    createRejilla(ctx, szPixel);
+    setColorsByMatriz(ctx, szPixel, matriz);
+}
+
+
+export function createRejilla(ctx, szPixel){
     const 
-        ctx = cv.getContext('2d'),
-        nCols = Math.floor(cv.width / szPixel.x),
-        nRows = Math.floor(cv.height / szPixel.y);
+        colorBoxes = colors.default_pixels,
+        nCols = Math.floor(ctx.canvas.width / szPixel.x),
+        nRows = Math.floor(ctx.canvas.height / szPixel.y);
     let color;
 
     for(let row=0; row < nRows; row++ ){
         //start color interleaved
-        color = row % 2 ? colors[0] : colors[1];
+        color = row % 2 ? colorBoxes[0] : colorBoxes[1];
 
         for(let col=0; col < nCols; col++ ){
-            color = color == colors[0] ? colors[1] : colors[0];
+            color = color == colorBoxes[0] ? colorBoxes[1] : colorBoxes[0];
 
             ctx.fillStyle = color;
             ctx.fillRect(col*szPixel.x, row*szPixel.y, szPixel.x, szPixel.y);
+        }
+    }
+}
+
+/**
+ * Imprime en el canvas una rejilla que contiene una cuadricula con las dimensiones dadas.
+ * @param {HTMLObjectElement} ctx Objeto canvas referido al html.
+ * @param { {x:int, y:int} } szPixel Objeto con las medidas que tendrá un pixel en x y y.
+ */
+export function setColorsByMatriz(ctx, szPixel, matriz){
+    //Tamaño de fuente que tendrá en mi canvas los números, relativo al tamaño del pixel:
+    const fontSize = szPixel.y - szPixel.y*0.18;
+    //Color del texto: [array con colores]
+    const colorText = colors.text;
+    //Tipografia:
+    ctx.font = 'bold '+fontSize + "px Times New Roman";
+    //Alineación
+    ctx.textAlign = "center";
+    
+    for(let row=0; row < matriz.length; row++){
+        for(let col=0; col < matriz[0].length; col++){
+            const value = matriz[row][col];
+            //Tamaño de un pixel:
+            const [x, y] = [szPixel.x, szPixel.y];
+            let afterActions = ()=>{};
+
+            switch(value){
+                case null:
+                    ctx.fillStyle = colors.noOpen;
+                break;
+
+                case 'b':
+                    const valPosition = row*10 + col;
+                    ctx.fillStyle = colors.bombs[ Math.floor( valPosition % colors.bombs.length ) ];
+                    afterActions = () => {
+                        ctx.fillStyle = '#0005';
+
+                        const img = new Image();
+                        img.src = 'img/circle_alpha.png';
+                        ctx.drawImage(img, col*x + 7, row*y + 7, x-14, y-14);
+
+                        /*ctx.arc(col*x + x/2, row*y + y/2, x - (x*.75), 0, Math.PI*2)
+                        ctx.fill();
+                        ctx.closePath();*/
+                    }
+                break;
+
+                case 'f':
+                    ctx.fillStyle = colors.noOpen;
+
+                    afterActions = () => {
+                        const img = new Image();
+                        img.src = 'img/flag.png';
+                        ctx.drawImage(img, col*x + 5, row*y + 5, x-10, y-10);
+                    }
+                break;
+
+                default:
+                    ctx.fillStyle = colors.open;
+
+                    afterActions = () => {
+                        if( typeof value == 'number' && value != 0){
+                            ctx.fillStyle = colorText[value];
+                            ctx.fillText(value, col*x + x/2, row*y + y/2 + szPixel.y*.3);
+                        }
+                    }
+            }
+
+            ctx.fillRect(col*x, row*y, x, y);
+            afterActions();
         }
     }
 }
